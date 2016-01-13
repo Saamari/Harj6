e148b9151423a1b07d5ee04f21d2270b2553211b
Saamari <sampsa_kautto@hotmail.com>
2016-01-13 15:08:31 +0200

    Harj6

---
+++ HArj6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HArj6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double matka;
+
+
+
+            Console.WriteLine("anna ajetun matkan määrä");
+
+            matka = double.Parse(Console.ReadLine());
+            double bensa = matka / 100 * 7.02;
+            double hinta = bensa * 1.595;
+
+            Console.WriteLine("bensaa mennyt {0}", bensa);
+            Console.WriteLine("hinta on {0:F}", hinta);
+        }
+    }
+}
